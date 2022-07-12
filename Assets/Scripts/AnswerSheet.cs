bd4244509f43d539e74b4e991db9efb042bb7088
Alon <Alonsouter@gmail.com>
2022-07-12 18:16:22 +0300

    A

---
+++ Assets/Scripts/AnswerSheet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerSheet : MonoBehaviour
+{
+    //held on patients - describes the actions required by the exam
+    public string patientID;
+    public List<string> actions;
+
+    private void Start()
+    {
+        FeedbackMaster.Instance.AddPatient(this);
+    }
+}
