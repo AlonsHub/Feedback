825cddc0d64ba9298325bc806e87eb27e4b8e9c2
Alon <Alonsouter@gmail.com>
2022-08-18 20:22:52 +0300

    YAS

--- Assets/Scripts/TreatmentGroup.cs
+++ Assets/Scripts/TreatmentGroup.cs
@@ -1,28 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[System.Serializable]
+[System.Serializable, CreateAssetMenu()]
 public class TreatmentGroup : SequenceBlock // a group of actions that may be performed in NO particular order
 {
-    List<Treatment> treatments; // => new List<Treatment>() with tests and questions
+    //List<Treatment> treatments; // => new List<Treatment>() with tests and questions
 
     [SerializeField]
-    List<TreatmentSO> treatmentSOs;
+    List<Treatment> treatments;
     //[SerializeField]
     //List<TestSO> testsSO;
     //[SerializeField]
     //List<QuestionSO> questionsSO;
 
-    public List<Treatment> GetTreatments()
-    {
-        treatments = new List<Treatment>();
-        foreach (var item in treatmentSOs)
-        {
-            treatments.Add(item.Treatment());
-        }
+    //public List<Treatment> GetTreatments()
+    //{
+    //    treatments = new List<Treatment>();
+    //    foreach (var item in treatmentSOs)
+    //    {
+    //        treatments.Add(item.Treatment());
+    //    }
          
-        return treatments;
-    }
+    //    return treatments;
+    //}
 
     public override bool WasPerformed()
     {
