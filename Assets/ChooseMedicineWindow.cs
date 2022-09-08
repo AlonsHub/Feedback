8d319c36f549900b8b69a1846d6c0412c8e889af
Alon <Alonsouter@gmail.com>
2022-09-08 20:17:14 +0300

    All treatment types can be created and added to patient
    Just need to setup the Treatment group (it's exactly the same process as the SequenceEditor - but uses <T> Treatment SO_Creator returned to the TreatmentGroup INSTEAD of TreatmentSequence

---
+++ Assets/ChooseMedicineWindow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChooseMedicineWindow : MonoBehaviour
+{
+    [SerializeField]
+    UnityEngine.UI.Toggle doClose;
+    [SerializeField]
+    TMP_Dropdown dropdown;
+    [SerializeField]
+    TMP_Text TEMP_patientData; //Look below.
+    //PatientMeasurementInput TBF!
+    //^^^ much like a displayer, this component will provide string input fields, corresponding to the parameters of patientMeasurementData.
+    //^^^ this UI prefab will then easily parse all input fields (each field to int/float/enum etc...) - to create a fresh patientMeasurementData.
+    //^^^ use this to initially set all patientMeasurementData on a new patient AND for medicine effect (as a "delta_patientMeasurementData" -> where only "fields to be changed" have value)
+
+
+    [SerializeField]
+    MedicineDB medicineDatabase;
+
+    [SerializeField]
+    TreatmentSequenceEditorWindow treatmentSequenceEditorWindow;
+
+    private void OnEnable()
+    {
+        if (!medicineDatabase)
+        {
+            Debug.LogError("Test database not found!");
+            return;
+        }
+
+        RefreshDropdownMedicine();
+    }
+
+    private void RefreshDropdownMedicine()
+    {
+        dropdown.ClearOptions();
+        dropdown.AddOptions(medicineDatabase.GetListOfTreatmentNames());
+        dropdown.RefreshShownValue();
+    }
+
+    public void OnClickAdd()
+    {
+        if (string.IsNullOrEmpty(TEMP_patientData.text))
+            return;
+
+        Medicine med = medicineDatabase.GetTreatmentByIndex(dropdown.value);
+        med.SetPatientData(TEMP_patientData.text); //This needs to just send patientMeasurementData tbf
+        //Set med's result (as PatientMeasurementData - taken from the PatientMeasurementInput TBF!
+
+        treatmentSequenceEditorWindow.AddTreatmentToSequence(med);
+        if (doClose.isOn)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/Medicine.cs
+++ Assets/Scripts/Medicine.cs
@@ -7,18 +7,30 @@ public class Medicine : Treatment
     [SerializeField]
     string medicineName; //with or without dosage (for display purposes only)
     //Currently dosage is part of the unique ID, so basically 10mg of drug X
+
+    /// <summary>
+    /// Should remain EMPTY for instances of Medcine that are in the Database.
+    /// Medicine as part of a treatment sequence should have a "patientMeasurementData" instructions as how to change a patients PatientMeasurementData
+    /// </summary>
     [SerializeField]
     object patientData;
 
-    //Medicine Setter TBF (also a creator? I dont think so)
+    [SerializeField]
+    string TEMP_patientDataString; //PLACEHOLDER FOR PatientMeasurementData
 
+    //Medicine Setter TBF (also a creator? I dont think so)
+    public void SetPatientData(string newDAta) //temp! TBF
+    {
+        TEMP_patientDataString = newDAta;
+    }
     public override object Result()
     {
-        return patientData;
+        //return patientData;
+        return TEMP_patientDataString;
     }
     public override string DisplayStringAsPartOfSequence()
     {
-        return $"שם התרופה:{medicineName}";
+        return $"שם התרופה:{medicineName} \n תגובה לתרופה:{TEMP_patientDataString}";
     }
     public override string TreatmentDisplayNameAsPartOfDatabase()
     {
